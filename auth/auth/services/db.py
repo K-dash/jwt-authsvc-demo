a35df1f0d59a1d2eef8350d9a43cda0916282bb5
K <51281148+K-dash@users.noreply.github.com>
2025-04-19 14:26:04 +0900

    コミット漏れ

--- auth/auth/services/db.py
+++ auth/auth/services/db.py
@@ -10,5 +10,6 @@ _async_engine = create_async_engine(settings.database_url, echo=False, pool_pre_
 _session_factory = async_sessionmaker(_async_engine, expire_on_commit=False, class_=AsyncSession)
 
 async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    """非同期セッションを取得する"""
     async with _session_factory() as session:
         yield session
